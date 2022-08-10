7e983bef7153564a19edaa310fa024755700e4e5
Pedro Ítalo Lobo <pedro@ongsys.com.br>
2022-08-10 19:01:38 -0300

    Primeros teste e função do projeto.

---
+++ src/presentation/routers/login-router.spec.js
@@ -0,0 +1,23 @@
+class LoginRouter{
+    route (httpRequest) {
+        if(!httpRequest.body.email){
+            return {
+                statusCode: 400,
+            } 
+        }
+    }
+}
+
+describe('Rota de login', () => {
+    test('Tem que retornar 400 se o não for enviado o email', () => {
+        const sut = new LoginRouter();
+        const httpRequest = {
+            body: {
+                senha: 'qualquer_senha'
+            }
+        };
+        const httpResponse = sut.route(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+
+    })
+})
