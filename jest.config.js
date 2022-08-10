44fe1370343a6893043720786f2462fd2ef9f92c
Pedro Ítalo Lobo <pedro@ongsys.com.br>
2022-08-10 19:01:11 -0300

    Nova config do jest.

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+
+const config = {
+  verbose: true,
+  coverageDirectory: "coverage",
+  testEnvironment: "node"
+};
+
+module.exports = config;
+
+module.exports = async () => {
+  return config;
+};
