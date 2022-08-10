1a5dc9d7ef25dc78169bf3308cd056fb960f411a
Pedro Ítalo Lobo <pedro@ongsys.com.br>
2022-08-10 20:01:25 -0300

    Atualizado os scripts, feitos outros testes.

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,15 +1,21 @@
 class LoginRouter{
     route (httpRequest) {
-        if(!httpRequest.body.email){
+        if(!httpRequest || !httpRequest.body){
             return {
-                statusCode: 400,
+                statusCode: 500
+            }
+        }
+        const { email, senha } = httpRequest.body
+        if(!email || !senha){
+            return {
+                statusCode: 400
             } 
         }
     }
 }
 
 describe('Rota de login', () => {
-    test('Tem que retornar 400 se o não for enviado o email', () => {
+    test('Tem que retornar status 400 se não for enviado o email', () => {
         const sut = new LoginRouter();
         const httpRequest = {
             body: {
@@ -18,5 +24,29 @@ describe('Rota de login', () => {
         };
         const httpResponse = sut.route(httpRequest);
         expect(httpResponse.statusCode).toBe(400);
+    })
+
+    test('Tem que retornar status 400 se não for enviado a senha', () => {
+        const sut = new LoginRouter();
+        const httpRequest = {
+            body: {
+                email: 'qualquer_email'
+            }
+        };
+        const httpResponse = sut.route(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+    })
+
+    test('Tem que retornar status 500 se não for enviado o request por paremetro', () => {
+        const sut = new LoginRouter();
+        const httpResponse = sut.route();
+        expect(httpResponse.statusCode).toBe(500);
+    })
+
+    test('Tem que retornar status 500 se não o request não tiver o body', () => {
+        const sut = new LoginRouter();
+        const httpRequest = {}
+        const httpResponse = sut.route(httpRequest);
+        expect(httpResponse.statusCode).toBe(500);
     })
 })
